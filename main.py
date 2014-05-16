dc8aaa07d42e868d2f43fa639a3d437e7dc37825
csalva <cristinaasalva@gmail.com>
2014-05-16 16:54:51 +0200

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,11 @@ menu()
 aplicaciones = str(input())
 
 if aplicaciones == 1
-    
+    print("1. Mostrar lista")
+    print("2. añadir nueva aplicacion")
+    print("3. modificar los datos de identificacion de un aplicacion")
+    print("4. sumar una descarga")
+    print("5. sumar un comentario")    
 
 elif aplicaciones == 2
     
