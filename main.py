9c82a997e816b1b8e97af35920f3c158cc8da547
csalva <cristinaasalva@gmail.com>
2014-05-16 17:09:44 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 __author__ = 'cristina'
+
+from aplicacionesFree import AplicacionesFree
+
 def menu():
     print("PeacheStore")
-    print("1-APLICACIONES GRATUITAS")
-    print("2-APLICACIONES DE PAGO")
-    
+    print("1.APLICACIONES GRATUITAS")
+    print("2.APLICACIONES DE PAGO")
+
 menu()
 aplicaciones = str(input())
 
@@ -12,7 +15,20 @@ if aplicaciones == 1
     print("2. añadir nueva aplicacion")
     print("3. modificar los datos de identificacion de un aplicacion")
     print("4. sumar una descarga")
-    print("5. sumar un comentario")    
+    print("5. sumar un comentario")
+
+    if tipoApp == 2:
+            print("Introduce Nombre Aplicacion")
+            nombreApp = str(input())
+            print("proveedor")
+            proveedorApp = str(input())
+            aplicacionesFree = AplicacionesFree(nombreApp,proveedorApp)
+            aplicacionesFree.crearRegistro()
+
 
 elif aplicaciones == 2
-    
+    print("1. Mostrar lista")
+    print("2. añadir nueva aplicacion")
+    print("3. modificar los datos de identificacion de un aplicacion")
+    print("4. sumar una descarga")
+    print("5. sumar un comentario")
