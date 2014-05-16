0126d54469df6bcd5a0f3b425d7a93066b973eb4
csalva <cristinaasalva@gmail.com>
2014-05-16 17:05:19 +0200

    Create aplicacionesFree.py

---
+++ aplicacionesFree.py
@@ -0,0 +1,18 @@
+__author__ = 'cristina'
+
+class AplicacionesFree():
+    def __init__(self, nombreApp,proveedorApp):
+        self.nombreApp = nombreApp
+        self.proveedorApp = proveedorApp
+
+    def getNombreApp(self):
+        return self.nombreApp
+
+    def getProveedorApp(self):
+        return self.proveedorApp
+
+        registroApp = (self.nombreApp+"-"+self.proveedorApp+"-"+date+"-"+"0,00"+"0"+"0"+"0"+"0"+"\n")
+
+        with open('aplicacionesFree.txt', mode='a', encoding='utf-8')as archivo:
+            archivo.write(registro)
+            print(registro)
