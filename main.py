8c37c9e5bc380a25c722483cbce47951d85b975f
csalva <cristinaasalva@gmail.com>
2014-05-16 16:38:50 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+__author__ = 'cristina'
+def menu():
+    print("PeacheStore")
+    print("1-APLICACIONES GRATUITAS")
+    print("2-APLICACIONES DE PAGO")
+    
+menu()
+aplicaciones = str(input())
+
+if aplicaciones == 1
+    
+
+elif aplicaciones == 2
+    
